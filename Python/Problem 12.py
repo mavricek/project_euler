e82189a31961164c4e07558aa96f0ad33699d48c
Matej <matej.mavricek@gmail.com>
2016-06-16 16:59:57 -0400

    Problem 12 troubleshoting

--- Python/Problem 12.py
+++ Python/Problem 12.py
@@ -6,10 +6,10 @@ from math import sqrt
 # Get the list of divisors
 def div_count(x):
 	divs = 0
-	for i in range(1,int(sqrt(x)+1)):
+	for i in range(1,int(x/2+1)):
 		if(x%i == 0):
 			divs += 1
-	return divs + 1
+	return divs + 1 #add one for x itself as factor
 
 # Get a method to create triangle numbers
 def triangle(x):
@@ -19,18 +19,18 @@ def triangle(x):
 	return tri_x
 
 def main():
-	x = 7
+	x = 6092
 	tri_x = triangle(x)
 	divs = div_count(triangle(x))
 
 	while divs < 500:
 		x += 1
-		tri_x = triangle(x)
+		tri_x += x
 		divs = div_count(tri_x)
-		print 'Divider count is %s' % divs
+		print 'Divider count of triangle number %s is %s' % (x, divs)
 
 	print 'Finished: triangle number of %s is %s with %s divisors' % (x, tri_x, div_count(tri_x))
 
 main()
 
-# Fina attempt interrupted at x = 3538
+# Last attempt interuppted at x = 6092
