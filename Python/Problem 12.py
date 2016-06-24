24e51ad828252a18bd2e0234ee88dcd01cfa9e87
Mavricek <matej.mavricek@gmail.com>
2016-06-23 22:17:45 -0400

    Problem 12 solved

--- Python/Problem 12.py
+++ Python/Problem 12.py
@@ -1,36 +1,44 @@
-import numpy as np
-from math import sqrt
+# import numpy as np
+# from math import sqrt
 
 # Get the first triangle number with 500 divisors
 
+
 # Get the list of divisors
 def div_count(x):
-	divs = 0
-	for i in range(1,int(sqrt(x)+1)):
-		if(x%i == 0):
-			divs += 1
-	return divs + 1
+    # From this: http://stackoverflow.com/questions/9076336/how-do-you-implement-the-divisor-function-in-code
+    divs = 0
+    sqroot = int(x ** 0.5)
+    for i in range(1, sqroot + 1):
+        if x % i == 0:
+            divs += 2  # both factor and N/factor
+    if sqroot * sqroot == x:
+        divs -= 1  # if sqroot is a factor then we counted it twice, so subtract 1
+    return divs
+
 
 # Get a method to create triangle numbers
 def triangle(x):
-	tri_x = 0
-	for j in range(x+1):
-		tri_x += j
-	return tri_x
+    tri_x = 0
+    for j in range(x + 1):
+        tri_x += j
+    return tri_x
+
 
 def main():
-	x = 7
-	tri_x = triangle(x)
-	divs = div_count(triangle(x))
+    x = 5
+    tri_x = triangle(x)
+    divs = div_count(triangle(x))
 
-	while divs < 500:
-		x += 1
-		tri_x = triangle(x)
-		divs = div_count(tri_x)
-		print 'Divider count is %s' % divs
+    while divs < 500:
+        x += 1
+        tri_x += x
+        divs = div_count(tri_x)
+        print 'Divider count is %s and x is %s' % (divs, x)
 
-	print 'Finished: triangle number of %s is %s with %s divisors' % (x, tri_x, div_count(tri_x))
+    print 'Finished: triangle number of %s is %s with %s divisors' % (x, tri_x, div_count(tri_x))
+
 
 main()
 
-# Fina attempt interrupted at x = 3538
+# Final attempt interrupted at x = 8043
