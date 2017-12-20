c5d0ebeb1561af41f3de5f6f134c5b76dbde0447
Matej <mmavricek@burning-glass.com>
2017-12-20 18:34:09 -0500

    no 13

--- Python/PE_13.py
+++ Python/PE_13.py
@@ -1,14 +1,21 @@
+# Euler Problem #13
+# Matej
+# 2017 November 30
+
 import numpy as np
 
-file_in = 'Problem 13 grid.txt'
-data = np.loadtxt(file_in)
+def main():
+	file_in = 'Problem 13 grid.txt'
+	data = np.loadtxt(file_in)
 
-sum = 0
+	sum = 0
 
-for row in data:
-    sum += row
-    print row
+	for row in data:
+	    sum += row
+	    print row
 
-sum_st = str(sum)
+	sum_st = str(sum)
 
-print sum_st[:11]
+	print sum_st[:11]
+
+main()
